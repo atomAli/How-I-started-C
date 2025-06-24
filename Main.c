d490ac836270840ef34747f055a03d7ed3bd0318
Ali Arjmandi <56292887+atomAli@users.noreply.github.com>
2025-06-24 16:18:35 +0200

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+// Function declarations
+int size_lesson(void);
+int data_types_lesson(void);
+int arithmetic_operations(void);
+int if_statements(void);
+int switch_statements(void);
+int temperature_conversion(void);
+int function(void);
+int arguments(void);
+int return_statement(void);
+int ternary_operator(void);
+int for_loop(void);
+int do_while_loop(void);
+int while_loop(void);
+int break_continue(void);
+int array(void);
+int twoD_array(void);
+int array_loop(void);
+int array_string(void);
+int swap_values(void);
+int sort_array(void);
+int structure(void);
+int type_def(void);
+int array_struct(void);
+int enums(void);
+int random_number(void);
+int game(void);
+int exercise1(void);
+int exercise2(void);
+int exercise3(void);
+int exercise4(void);
+int exercise5(void);
+int exercise6(void);
+int exercise7(void);
+int function_array(void);
+int exercise8(void);
+int main(void) {
+    printf("Running ...\n");
+    exercise8();
+    return 0;
+}
