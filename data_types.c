2a1a8cf793496f90744629c6e4f811430e2e8041
Ali Arjmandi <56292887+atomAli@users.noreply.github.com>
2025-06-25 18:15:28 +0200

    Create data_types.c

---
+++ data_types.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int data_types_lesson(void) {
+  // Exercise 1:
+  // Create a variable of type int and assign it the value 10.
+  // Print the value of the variable.
+  int A = 22;
+  printf("%d\n", A);
+
+  // Exercise 2:
+  // Create a variable of type char and assign it the value 'A'.
+  // Print the value of the variable.
+  char B = 'A';
+  printf("%c\n", B);
+  // Exercise 3:
+  // Create a variable of type float and assign it the value 3.14.
+  // Print the value of the variable.
+  float C = 18.5;
+  printf("%f\n", C);
+  // Exercise 4:
+  // Create a variable of type double and assign it the value 2.71828.
+  // Print the value of the variable.
+  double D = 243.837487;
+  printf("%lf\n", D);
+  // Exercise 5:
+  // Create a variable of type bool and assign it the value true.
+  // Print the value of the variable.
+  bool E = true;
+  printf("%d\n", E);
+  
+
+  return 0;
+}
